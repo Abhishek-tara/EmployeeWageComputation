d701668944a3e697991a3f308e66c3da6141dfc2
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-09 11:23:10 +0530

    Using Switch Case to Calculate

--- CheckEmployee.cs
+++ CheckEmployee.cs
@@ -8,11 +8,12 @@ namespace EmployeeWageComputation
 {
     public class CheckEmployee
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         public static void EmployeeCheck()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+           
 
             int empHrs = 0;
             int empWage = 0;
@@ -20,17 +21,20 @@ namespace EmployeeWageComputation
             Random random = new Random();
             int empCheck = random.Next(3);
 
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if(empCheck == IS_PART_TIME)
-            {
-                empHrs= 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs= 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
+                    
+                
             }
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
