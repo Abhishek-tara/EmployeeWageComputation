8ddde3fcd51b66ce1b5993206c1331fc044f7c13
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-09 08:46:01 +0530

    Calling Employee Check program

---
+++ CheckEmployee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class CheckEmployee
+    {
+        public static void EmployeeCheck()
+        {
+            int IS_FULLTIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULLTIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            
+
+        }
+
+
+    }
+}
